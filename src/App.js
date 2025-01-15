dd02a6020720c8e9944f55b864f59d8af6c4c84e
Rajae Laalalma <rajaerajaela@gmil.com>
2025-01-16 00:04:18 +0100

    update-2  project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import NavBar from "./components/NavBar";
 
 
 
 function App() {
   return (
     <div className="App">
-  <h1>hello </h1>
+  <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div >
+     <div className='w-full h-24 text-white border-slate-600 bg-black flex justify-center items-center'> hhh
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
