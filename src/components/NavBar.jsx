80f5cd75831731fec9e825f00a7bb843d453ac57
Rajae Laalalma <rajaerajaela@gmil.com>
2025-01-16 00:24:33 +0100

    update-NavBar  project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
 const NavBar = () => {
+    
+    const Links=[
+        {
+              id:1,
+        Link:'Home'
+
+        },
+        {
+            id:2,
+      Link:'About'
+
+      },
+      {
+        id:3,
+  Link:'About'
+
+  }
+    ]
   return (
     <div >
-     <div className='w-full h-24 text-white border-slate-600 bg-black flex justify-center items-center'> hhh
+     <div className='w-full h-24 border border-red-600  flex justify-between  items-center px-4'>
+        <div>Rajae Laalalma</div>
+        <ul className='flex  justify-between items-center  border border-red-600'>
+            {
+                Links.map(({id,Link})=>(
+                     <li key={id} className='px-4 '>{Link}</li>
+                ))
+            }
+           
+            </ul>
         </div>
+
     </div>
   )
 }
