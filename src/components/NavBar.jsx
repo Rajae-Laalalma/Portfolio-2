3bade02e8f94184145eec804d56f56a8b3e3693f
Rajae Laalalma <rajaerajaela@gmil.com>
2025-01-20 13:12:34 +0100

    NavBar termine  project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -52,21 +52,33 @@ Link:'Projects'
         
             </ul>
 
-            <ul className='hidden md:flex items-center justify-center'>
+            <ul className='hidden md:flex items-center justify-center '>
               <li className=' pl-6  '>
-                <span className="text-gray-500 hover:text-violet-300 ">
-  <GrGithub size={25} className=''/>
+                <span className="text-gray-500 hover:text-violet-300  ">
+  <GrGithub size={25} className=' transform transition-transform duration-300 hover:scale-125'/>
 </span> </li>
 <span>
-  <li className='flex items-center justify-center w-6 h-6 m-2 rounded-full bg-gray-500 hover:bg-violet-300 text-white'><GrLinkedinOption className='w-4 h-4' /></li>
+  <li className='flex items-center justify-center w-6 h-6 m-2 rounded-full bg-gray-500 hover:bg-violet-300 text-white  transform transition-transform duration-300 hover:scale-125 '><GrLinkedinOption className='w-4 h-4' /></li>
 </span>
            
             </ul>
-            <div onClick={()=>setNav(!nav)} className='md:hidden '>
+            <div onClick={()=>setNav(!nav)} className='md:hidden z-10'>
               {nav?<FaTimes size={25} className='text-gray-500' />:<FaBars size={25} className='text-gray-500'/>}
             
-
             </div>
+            {nav &&(
+                 <ul className=' md:hidden  flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-r from-blue-500 to-violet-500 '>
+            {
+                Links.map(({id,Link})=>(
+                     <li key={id} className='py-4 text-white cursor-pointer font-title text-2xl'>{Link}</li>
+                ))
+            }
+          
+             
+            
+            </ul>
+            )}
+         
         </div>
 
     </div>
