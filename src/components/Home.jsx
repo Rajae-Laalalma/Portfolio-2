79688248e20962d2e265f4bca5e5a88ef058d082
Rajae Laalalma <rajaerajaela@gmil.com>
2025-01-20 16:15:04 +0100

    Update Home  project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,20 @@ import profile from '../assets/profile.jpg'
 
 const Home = () => {
   return (
-    <div className='flex flex-row justify-center items-center  w-full h-screen  '>
-        <div className='  text-4xl font-title font-bold text-blue-900 pr-40'>
+    <div className='md:flex md:flex-row  justify-center items-center  w-full h-screen px-10 py-10 '>
+        <div className='  text-4xl font-title font-bold text-blue-900 md:pr-40'>
             
             <h1 className=''>Hi 👋,</h1>
 <h1 className='uppercase py-2'>My name is</h1>
 <span className='uppercase py-2 text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500'>Rajae Laalalma</span>
 <h1 className='uppercase py-2'>I build things for web</h1>
-<div className='py-4'>
+<div className='md:py-4 py-8 '>
    <button className='text-xl  text-white bg-gradient-to-r from-blue-500 to-violet-500 w-32  h-11 rounded font-normal transform transition-transform duration-300 hover:scale-125'>Portfolio</button> 
 </div>
 
 </div>
 
-      <div className='w-80 h-80 rounded-full'><img src={profile} alt=""  className=' w-80 h-80  rounded-full border-4 border-transparent bg-clip-border bg-gradient-to-r from-blue-500 to-violet-600  '/></div>
+      <div className='w-80 h-80 rounded-full mx-40 md:mx-8  '><img src={profile} alt=""  className=' w-80 h-80  rounded-full border-4 border-transparent bg-clip-border bg-gradient-to-r from-blue-500 to-violet-600  '/></div>
     </div>
   )
 }
