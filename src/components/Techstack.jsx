7830721f3501c9b9f10a8b560ecc916e3e2f3669
Rajae Laalalma <rajaerajaela@gmil.com>
2025-01-23 19:50:00 +0100

    Update Techstack  project

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -10,31 +10,51 @@ import Vscode from '../assets/visual-studio-code.png'
 import mongodb from '../assets/mongodb.png'
 import { GrGithub } from "react-icons/gr";
 
-
 const Techstack = () => {
+  
   return (
-    <div className=' w-full h-screen bg-neutral-300 '>
+    <div className='w-full md:h-screen h-50 bg-neutral-300 flex flex-col items-center'>
       <div>
-        <h1 className='pt-10 text-4xl text-blue-900 font-title font-bold flex justify-center '> My Tech Stack</h1>
-        <p className='flex justify-center text-2xl text-gray-500 py-4'> Technologies I’ve been working with recently</p>
-        </div>
+        <h1 className='pt-10 text-4xl text-blue-900 font-title font-bold flex justify-center'>My Tech Stack</h1>
+        <p className='flex justify-center text-2xl text-gray-500 py-4'>Technologies I’ve been working with recently</p>
+      </div>
 
-         <div className='flex  w-40 h-40 border border-red-500'> 
-      <img src={html} alt="" className=' border border-red-500 w-40 h-40 ' />
-      <img src={css} alt="" className='border border-red-500' />
-      <img src={javascript} alt="" className='border border-red-500'/>
-      <img src={react} alt="" className='border border-red-500'/>
-      <img src={bootstrap} alt="" className='border border-red-500'/>
-      <img src={tailwindcss} alt="" className='border border-red-500'/>
-      
-
-     </div>
      
-      <div className='flex'>
-      <img src={git} alt="" />
-      <img src={Vscode} alt="" />
-       <GrGithub size={150} className=''/>
-      <img src={mongodb} alt="" />
+      <div className='flex flex-wrap justify-center items-center gap-6 mt-6'>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <img src={html} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125 '>
+          <img src={css} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <img src={javascript} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <img src={react} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <img src={bootstrap} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125 '>
+          <img src={tailwindcss} alt="" className='w-32 h-32' />
+        </div>
+      </div>
+
+    
+      <div className='flex flex-wrap justify-center items-center gap-6 mt-8'>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <img src={git} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <img src={Vscode} alt="" className='w-32 h-32' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125'>
+          <GrGithub size={80} className='text-gray-800' />
+        </div>
+        <div className='flex justify-center items-center w-40 h-40 transform transition-transform duration-300 hover:scale-125 '>
+          <img src={mongodb} alt="" className='w-32 h-32' />
+        </div>
       </div>
     </div>
   )
