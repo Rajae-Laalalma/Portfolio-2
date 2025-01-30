e7c1de2d4510510924c46217701a0fb76904a5a1
Rajae Laalalma <rajaerajaela@gmil.com>
2025-01-30 21:12:26 +0100

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
@@ -14,6 +15,7 @@ function App() {
       <Techstack />
       <Projects />
       <Contacts />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-32 bg-violet-600'>
+        <div></div>
+        <div></div>
+      
+    </div>
+  )
+}
+
+export default Footer
